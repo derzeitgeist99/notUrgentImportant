6bfd6fb5b085d85a3aa21a4612a44bd862b81ece
derZeitgeist@zeitgeist.com <andy@Andrejs-MacBook-Air.local>
2022-05-07 18:40:14 +0200

    Added Edit Tags in UpdateList

---
+++ src/Components/TagIcons.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { FaCircle } from "react-icons/fa"
+import { useTaskData } from "../Context/TaskDataContext";
+import { palette } from "../Styled/theme"
+
+
+export function TagIcons({ taskIndex, tagColor, tagIndex, renderCounter, setRenderCounter }) {
+    const [taskData, setTaskData] = useTaskData()
+    // This has to be centrally controlled
+    const [availableTags] = useState([0, 1, 2, 3])
+    const [colors, setColors] = useState([])
+    // This has to come from context
+    const userSettings = { palette: "clean" }
+
+    useEffect(() => {
+        setColors(palette[userSettings.palette])
+    }, [])
+
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        // send to Airtable
+        const payload = { ...taskData[taskIndex], Tag: event.currentTarget.dataset.div_id }
+        //update Context
+        const newTaskData = taskData
+        newTaskData[taskIndex] = payload
+        setTaskData(newTaskData)
+        // Workaround for renderint the UpdateList js. see more notes there
+        setRenderCounter(renderCounter += 1)
+    }
+
+
+    return (
+        availableTags.map((tag) => (
+            <FaCircle style={{ "color": colors[tag], cursor: "pointer" }}
+                onClick={(event) => handleClick(event)}
+                key={tag}
+                data-div_id={tag} //https://ozmoroz.com/2018/07/pass-value-to-onclick-react/
+            />)
+        )
+    )
+}
--- src/Pages/UpdateList.js
+++ src/Pages/UpdateList.js
@@ -3,6 +3,7 @@ import EditTask from "../Components/EditTask";
 import { StyledContainer, StyledEditControlsDiv, StyledTask, StyledTaskBox } from "../Styled/StyledTaskList";
 import { AiOutlineEdit } from "react-icons/ai"
 import { useTaskData } from "../Context/TaskDataContext";
+import { TagIcons } from "../Components/TagIcons";
 
 
 
@@ -14,16 +15,20 @@ export default function UpdateTaksList() {
         { id: 4, "Text": "Shave", Tag: 3 }]
     const userSettings = { palette: "clean" }
     const [isEdit, setIsEdit] = useState(null)
-    const [tasks, setTasks] = useTaskData(dummydata)
+    const [tasks, setTasks] = useTaskData()
+    //This is silly, but I cannot get the color to change after TagIcon change
+    // The context is changing, but this component is not registering the change
+    // works with editTask component
+    const [renderCounter, setRenderCounter] = useState(0)
 
     useEffect(() => {
         setTasks(dummydata)
     }, [])
 
+
     const handleEditButton = (event, taskId) => {
         event.preventDefault()
         setIsEdit(taskId)
-
     }
 
 
@@ -35,12 +40,11 @@ export default function UpdateTaksList() {
                 <div className="" key={task.id}>
                     {(isEdit != task.id) ?
                         <StyledTaskBox>
-
                             <StyledTask
-
                                 taskColor={task.Tag}
                                 palette={userSettings.palette} >{task.Text}</StyledTask>
                             <StyledEditControlsDiv>
+                                <TagIcons taskIndex={index} tagIndex={0} renderCounter={renderCounter} setRenderCounter={setRenderCounter} />
                                 <AiOutlineEdit onClick={(event) => handleEditButton(event, task.id)} style={{ "color": "blue", "cursor": "pointer" }} />
                             </StyledEditControlsDiv>
                         </StyledTaskBox>
