9b9f24abebf2cd5fcfd71992945ae4d75b66bdff
derzeitgeist.eth <derzeitgeist99@icloud.com>
2022-06-17 16:32:37 +0200

    moved edit pencil

--- src/Components/MainList/StyledMainList/StyledTaskList.js
+++ src/Components/MainList/StyledMainList/StyledTaskList.js
@@ -11,11 +11,10 @@ justify-content: flex-start;
 
 `
 export const StyledTaskBox = styled.div`
-
-
 margin-bottom: 1rem;
 display: grid;
-grid-template-rows: 120px auto;
+grid-template-rows: 100px 20px;
+grid-template-columns: auto 20px;
 `
 
 export const StyledTask = styled.div`
@@ -23,12 +22,17 @@ background-color: ${props => props.backgroundColor} ;
 margin: 0.0rem;
 padding: 2rem;
 font-size: 1rem;
+grid-row: 1 /3 ;
+grid-column: 1/3;
+
 
 `
 
 
 export const StyledEditControlsDiv = styled.div`
-padding: 0;
-background-color: white;
+padding: 0.2rem;
+place-self: center;
+grid-row: 2/3 ;
+grid-column: 2/3 ;
 
 `
